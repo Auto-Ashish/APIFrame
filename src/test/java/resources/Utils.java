44b6cc7838772207c38dba98cb65e54956e04b6d
Auto-Ashish <121753830+Auto-Ashish@users.noreply.github.com>
2023-07-13 19:46:50 +0530

    Update Utils.java

--- src/test/java/resources/Utils.java
+++ src/test/java/resources/Utils.java
@@ -40,7 +40,9 @@ public class Utils {
 	public static String getGlobalValue(String key) throws IOException
 	{
 		Properties prop =new Properties();
-		FileInputStream fis =new FileInputStream("\\src\\test\\java\\resources\\global.properties");
+		String path = System.getProperty("user.dir");
+       		 System.out.println(path);
+		FileInputStream fis =new FileInputStream(path+\\src\\test\\java\\resources\\global.properties");
 		prop.load(fis);
 		return prop.getProperty(key);
 	
